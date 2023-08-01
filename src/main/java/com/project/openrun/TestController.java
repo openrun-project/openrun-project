abc4e5992c3a25006b1a603f9a032eed47dfe98b
hongsh429 <shh0429@kakao.com>
2023-08-01 13:41:56 +0900

    test : docker 로그 확인하기

--- src/main/java/com/project/openrun/TestController.java
+++ src/main/java/com/project/openrun/TestController.java
@@ -1,14 +1,17 @@
 package com.project.openrun;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class TestController {
 
     @RequestMapping("/test")
     public String test() {
+        log.info("TestController test log 확인");
         return "hello, CICD";
     }
 }
