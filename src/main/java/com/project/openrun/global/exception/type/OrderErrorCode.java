011366ced312e99534fbc2ceaf59273b4dfa3e61
siwoo1224 <96398475+siwoo1224@users.noreply.github.com>
2023-08-03 22:31:54 +0900

    feat : order Testcode 작성

--- src/main/java/com/project/openrun/global/exception/type/OrderErrorCode.java
+++ src/main/java/com/project/openrun/global/exception/type/OrderErrorCode.java
@@ -8,9 +8,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum OrderErrorCode {
 
-    NOT_ORDER(HttpStatus.BAD_REQUEST,"주문 내역이 없습니다."),
+    NOT_ORDER(HttpStatus.BAD_REQUEST,"주문 내역이 존재하지 않습니다."),
     NOT_PRODUCT(HttpStatus.BAD_REQUEST, "해당 상품이 존재하지 않습니다."),
-    NOT_USER_ORDER(HttpStatus.BAD_REQUEST, "삭제할 권한이 없습니다.");
+    NOT_USER_ORDER(HttpStatus.BAD_REQUEST, "주문을 취소할 권한이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String errorMsg;
