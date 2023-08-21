1b2202ec4eb197e5933fffe62f909f11eff11cb8
이규행 <2kyuhang@naver.com>
2023-08-22 00:37:27 +0900

    issue : 오픈런 검색 페이지 수정 #58

--- src/main/resources/static/js/openrun.js
+++ src/main/resources/static/js/openrun.js
@@ -14,6 +14,62 @@ $('#pagination').on('click', 'a', function (e) {
 
 
 
+function searchProducts(page) {
+    let size = 16;
+    let value = (page == null || page === "") ? 0 : page;
+
+    //검색어
+    var keyword = $("#search-input").val();
+    var category = $("#category").val();
+    var sortBy = $("#array").val();
+    var isAsc = $("#array option:selected").text().endsWith("△");
+    var minPrice = $("#minPrice").val();
+    var maxPrice = $("#maxPrice").val();
+
+    if (keyword === '' && category === '' && sortBy === '' && minPrice === '' && maxPrice === '') {
+        console.log("검색 조건을 입력해주세요.");
+        return;
+    }
+
+    $.ajax({
+        type: "GET",
+        url: `/api/products/search`,
+        data: {
+            size : size,
+            page : value,
+            keyword: keyword,
+            category: category,
+            sortBy: sortBy,
+            lprice: minPrice,
+            gprice: maxPrice,
+            isAsc: isAsc
+        },
+        dataType: "json",
+        contentType: "json",
+        success: function (data) {
+            console.log(data)
+
+            currentPage = data.number;
+            totalPages = data.totalPages;
+            console.log("data['content'].length()", data['content'].length)
+            if(data['content'].length === 0){
+                var productList = $('#product-row');
+                productList.empty();
+                let html = `<h1 style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"> 검색 결과가 없습니다.</h1>`;
+                productList.append(html);
+                updatePagination2();
+                return;
+            }
+            displayData(data['content']);
+            updatePagination2();
+        },
+        error: function () {
+            alert("실패");
+        }
+    });
+}
+
+
 function fetchProducts(page) {
     let size = 16;
     $.ajax({
@@ -92,6 +148,37 @@ function updatePagination() {
     /* >> */
     if (endPage < totalPages - 1) {
         pagination.append('<a href="#" onclick="fetchProducts(' + (endPage + 1) + ')">&raquo;</a>');
+    }
+}
+
+
+function updatePagination2() {
+    var pagination = $('#pagination');
+    pagination.empty();
+
+    /*현재 페이지 번호를 기준으로 페이징의 시작 페이지를 계산합니다. 예를 들어, 현재 페이지가 15라면 startPage는 10이 됩니다.*/
+    var startPage = Math.floor(currentPage / 10) * 10;
+    /*페이징의 끝 페이지를 계산합니다. startPage에서 9를 더한 값과 totalPages - 1 중 작은 값을 선택합니다. 이렇게 하면 최대 10개의 페이지 버튼이 표시됩니다.*/
+    var endPage = Math.min(startPage + 9, totalPages - 1);
+
+    /* << */
+    if (startPage > 0) {
+        pagination.append('<a href="#" onclick="searchProducts(' + (startPage - 1) + ')">&laquo;</a>');
+    }
+
+    for (var i = startPage; i <= endPage; i++) {
+        if (i === currentPage) {
+            pagination.append('<span class="current-page">[' + (i + 1) + ']</span>');
+        } else {
+            pagination.append('<a href="#">' +
+                '[' + (i + 1) + ']' +
+                '</a>');
+        }
+    }
+
+    /* >> */
+    if (endPage < totalPages - 1) {
+        pagination.append('<a href="#" onclick="searchProducts(' + (endPage + 1) + ')">&raquo;</a>');
     }
 }
 
