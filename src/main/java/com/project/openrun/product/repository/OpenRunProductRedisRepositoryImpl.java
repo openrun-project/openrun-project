7c88254ad1801e22c4fa5a986a59e32e9ee30d58
KIM MIN GYU <mingyugim584@gmail.com>
2023-08-30 00:35:46 +0900

    LocalDateTime Json 자동 변환 설정

--- src/main/java/com/project/openrun/product/repository/OpenRunProductRedisRepositoryImpl.java
+++ src/main/java/com/project/openrun/product/repository/OpenRunProductRedisRepositoryImpl.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Repository;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.Optional;
 
 @Repository
