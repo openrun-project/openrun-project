33fdb55e6caa26f40b31363bfed31155dcca3468
KIM MIN GYU <mingyugim584@gmail.com>
2023-08-30 20:14:26 +0900

    ItemReader 페이징 방식 테스트

--- src/main/java/com/project/openrun/batch/ProductJobConfig.java
+++ src/main/java/com/project/openrun/batch/ProductJobConfig.java
@@ -18,8 +18,8 @@ import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.database.JpaCursorItemReader;
-import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,6 +27,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import java.time.LocalDate;
@@ -97,6 +99,9 @@ public class ProductJobConfig {
                         currentPage++;
                     }
                 })
+                .faultTolerant()
+                .retry(Exception.class)
+                .retryLimit(3)
                 .build();
     }
 
@@ -109,59 +114,80 @@ public class ProductJobConfig {
     }
 
 
-//    @Bean
-//    public Step openRunSaveRedisStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
-//        return new StepBuilder("openRunSaveRedisStep", jobRepository)
-//                .tasklet(openRunSaveRedisStepTasklet(), platformTransactionManager)
-//                .build();
-//    }
-
-
     @Bean
     public Tasklet openRunUpdateStepTasklet() {
-        return (contribution, chunkContext) -> {
-            LocalDateTime yesterday = LocalDate.now().plusDays(-1).atStartOfDay();//어제
-            LocalDateTime today = LocalDate.now().atStartOfDay();//오늘
-            LocalDateTime tomorrow = LocalDate.now().plusDays(1).atStartOfDay();//내일
+        RetryTemplate retryTemplate = new RetryTemplate();
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+        retryPolicy.setMaxAttempts(3); // 최대 3회 재시도
+        retryTemplate.setRetryPolicy(retryPolicy);
 
-            try {
-                productRepository.updateProductStatus(yesterday, today, OpenRunStatus.CLOSE, OpenRunStatus.OPEN);//OPEN => CLOSE
-                productRepository.updateProductStatus(today, tomorrow, OpenRunStatus.OPEN, OpenRunStatus.WAITING);// WAITING => OPEN
-            } catch (Exception e) {
-                log.error(e.getMessage());
-            }
-            return RepeatStatus.FINISHED;
+        return (contribution, chunkContext) -> {
+            return retryTemplate.execute(retry -> {
+                        LocalDateTime yesterday = LocalDate.now().plusDays(-1).atStartOfDay();//어제
+                        LocalDateTime today = LocalDate.now().atStartOfDay();//오늘
+                        LocalDateTime tomorrow = LocalDate.now().plusDays(1).atStartOfDay();//내일
+
+                        productRepository.updateProductStatus(yesterday, today, OpenRunStatus.CLOSE, OpenRunStatus.OPEN);//OPEN => CLOSE
+                        productRepository.updateProductStatus(today, tomorrow, OpenRunStatus.OPEN, OpenRunStatus.WAITING);// WAITING => OPEN
+
+                        return RepeatStatus.FINISHED;
+                    }, context -> {
+                        log.error("Failed after 3 retries!");
+                        return RepeatStatus.FINISHED;
+                    }
+            );
         };
     }
 
 
     @Bean
     public Tasklet openRunCountStepTasklet() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+        retryPolicy.setMaxAttempts(3); // 최대 3회 재시도
+        retryTemplate.setRetryPolicy(retryPolicy);
+
         return (contribution, chunkContext) -> {
 
-            Long count = productRepository.countByStatus(OpenRunStatus.OPEN);
-            openRunProductRedisRepository.saveProductCount(count);
+            return retryTemplate.execute(retry -> {
+                Long count = productRepository.countByStatus(OpenRunStatus.OPEN);
+                openRunProductRedisRepository.saveProductCount(count);
 
-            // count 값 BATCH JOB EXECUTION CONTEXT 에 넣어주기.
-            chunkContext.getStepContext()
-                    .getStepExecution()
-                    .getJobExecution()
-                    .getExecutionContext()
-                    .put("count", count);
+                // count 값 BATCH JOB EXECUTION CONTEXT 에 넣어주기.
+                chunkContext.getStepContext()
+                        .getStepExecution()
+                        .getJobExecution()
+                        .getExecutionContext()
+                        .put("count", count);
 
-            return RepeatStatus.FINISHED;
+                return RepeatStatus.FINISHED;
+            }, context -> {
+                log.error("Failed after 3 retries!");
+                return RepeatStatus.FINISHED;
+            });
+
         };
     }
 
     @Bean
-    public JpaCursorItemReader<Product> openRunSaveRedisItemReader() {
+    public JpaPagingItemReader<Product> openRunSaveRedisItemReader() {
 
-        return new JpaCursorItemReaderBuilder<Product>()
+        return new JpaPagingItemReaderBuilder<Product>()
                 .name("openRunSaveRedisItemReader")
                 .entityManagerFactory(entityManagerFactory)
                 .queryString("select p from Product p where status= 'OPEN' order by id desc")
+                .pageSize(16)
                 .build();
     }
+//    @Bean
+//    public JpaCursorItemReader<Product> openRunSaveRedisItemReader() {
+//
+//        return new JpaCursorItemReaderBuilder<Product>()
+//                .name("openRunSaveRedisItemReader")
+//                .entityManagerFactory(entityManagerFactory)
+//                .queryString("select p from Product p where status= 'OPEN' order by id desc")
+//                .build();
+//    }
 
     @Bean
     public ItemProcessor<Product, OpenRunProductResponseDto> openRunSaveRedisItemProcessor() {
@@ -187,12 +213,22 @@ public class ProductJobConfig {
 
     @Bean
     public Tasklet saveAllProductCountStepTasklet() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+        retryPolicy.setMaxAttempts(3); // 최대 3회 재시도
+        retryTemplate.setRetryPolicy(retryPolicy);
+
         return (contribution, chunkContext) -> {
 
-            Long count = productRepository.count();
-            allProductRedisRepository.saveProductCount(count);
+            return retryTemplate.execute(retry -> {
+                Long count = productRepository.count();
+                allProductRedisRepository.saveProductCount(count);
 
-            return RepeatStatus.FINISHED;
+                return RepeatStatus.FINISHED;
+            }, context -> {
+                log.error("Failed after 3 retries!");
+                return RepeatStatus.FINISHED;
+            });
         };
     }
 
