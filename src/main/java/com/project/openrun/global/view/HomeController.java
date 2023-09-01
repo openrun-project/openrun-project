f9343b6813eda2e44fd8ca5e51fbfafded5bcb94
hongsh429 <shh0429@kakao.com>
2023-09-02 00:50:35 +0900

    healthcheck

--- src/main/java/com/project/openrun/global/view/HomeController.java
+++ src/main/java/com/project/openrun/global/view/HomeController.java
@@ -1,7 +1,9 @@
 package com.project.openrun.global.view;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class HomeController {
@@ -9,5 +11,10 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "redirect:/openrun/main";
+    }
+
+    @GetMapping("/health-check")
+    @ResponseStatus(HttpStatus.OK)
+    public void check() {
     }
 }
